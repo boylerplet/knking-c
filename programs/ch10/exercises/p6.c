fceb18749bedc044c63ebb11f8503ae4f41b4298
boylerplet <amrutwali@gmail.com>
2024-09-28 12:44:48 +0530

    Update chapter 10

--- programs/ch10/exercises/p6.c
+++ programs/ch10/exercises/p6.c
@@ -1,11 +1,128 @@
 /* Name: p6.c
  * Author: Amrut Wali
- * Description: 
+ * Description: Reverse Polish Notation
  */
 
+// +, -, *, /, =, where = shows the top item in the stack
+
+#include <stdbool.h>
 #include <stdio.h>
+
+#define STACK_SIZE 100
+
+/* external variables */
+int contents[STACK_SIZE];
+int top = 0;
+
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(int i);
+int pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
+
+void print_stack(void)
+{
+    printf("Stack: |");
+    for (int i = 0; i <= top; i++) {
+        printf(" %d |", contents[i]);
+    }
+    printf("\n");
+}
 
 int main(void)
 {
-	return 0;
+    int x, y, result;
+    int term;
+    char ch;
+
+    printf("Enter RPN expression: ");
+
+    // Read expression and calculate value
+    while ((ch = getchar()) != '\n') {
+        switch (ch) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0': push(ch - '0'); break;
+            case '+':
+                y = pop();
+                x = pop();
+                term = x + y;
+                push(term);
+                break;
+            case '-':
+                y = pop();
+                x = pop();
+                push(x - y);
+                break;
+            case '*':
+                y = pop();
+                x = pop();
+                push(x * y);
+                break;
+            case '/':
+                y = pop();
+                x = pop();
+                push(x / y);
+                break;
+            case '=':
+                result = pop();
+                break;
+            case ' ': continue;
+            default:  printf("Wrong expression\n"); break;
+        }
+    };
+
+    printf("The value of the expression is: %d", result);
+
+    return 0;
+}
+
+void make_empty(void)
+{
+    top = 0;
+}
+
+bool is_empty(void)
+{
+    return top == 0;
+}
+
+bool is_full(void)
+{
+    return top == STACK_SIZE;
+}
+
+void push(int i)
+{
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = i;
+}
+
+int pop(void)
+{
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+}
+
+void stack_overflow()
+{
+    printf("Stack Overflow!!!\n");
+}
+
+void stack_underflow()
+{
+    printf("Stack Overflow!!!\n");
 }
--- programs/ch10/exercises/p7.c
+++ programs/ch10/exercises/p7.c
@@ -1,11 +1,85 @@
 /* Name: p7.c
  * Author: Amrut Wali
- * Description: 
+ * Description: 7 Segment display
  */
 
+#include <ctype.h>
 #include <stdio.h>
+
+#define NUMBER_OF_DIGITS 10
+#define SEGMENTS 7
+#define MAX_NUMBERS 13
+
+void print_number(int numbers[][*], int i);
+void print_numbers(int numbers[][*]);
+void update_number(int numbers[MAX_NUMBERS][SEGMENTS], int i, int num);
+
+int digits[NUMBER_OF_DIGITS][SEGMENTS] = {
+    {1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 0, 0, 0, 0}, {1, 1, 0, 1, 1, 0, 1},
+    {1, 1, 1, 1, 0, 0, 1}, {0, 1, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 0, 1, 1},
+    {1, 0, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 0, 0, 0}, {1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 0, 1, 1}};
 
 int main(void)
 {
-	return 0;
+    int numbers[MAX_NUMBERS][SEGMENTS] = {
+        {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0}};
+    int i = 0;
+    char ch;
+
+    printf("Enter a number: ");
+    while ((ch = getchar()) != '\n') {
+        if (i < 10) {
+            if (isdigit(ch)) {
+                update_number(numbers, i, ch - '0');
+                i++;
+            }
+        } else {
+            break;
+        }
+    }
+
+    print_numbers(numbers);
+
+    return 0;
+}
+
+void update_number(int numbers[MAX_NUMBERS][SEGMENTS], int i, int num)
+{
+    for (int j = 0; j < SEGMENTS; j++) {
+        numbers[i][j] = digits[num][j];
+    }
+}
+
+void print_number(int numbers[MAX_NUMBERS][SEGMENTS], int i)
+{
+    printf(" %c \n", numbers[i][0] == 0 ? ' ' : '_');
+    printf("%c%c%c\n", numbers[i][5] == 0 ? ' ' : '|',
+           numbers[i][6] == 0 ? ' ' : '_', numbers[i][1] == 0 ? ' ' : '|');
+    printf("%c%c%c\n", numbers[i][4] == 0 ? ' ' : '|',
+           numbers[i][3] == 0 ? ' ' : '_', numbers[i][2] == 0 ? ' ' : '|');
+};
+
+void print_numbers(int numbers[MAX_NUMBERS][SEGMENTS])
+{
+    // print first line
+    for (int x = 0; x < MAX_NUMBERS; x++) {
+        printf(" %c  ", numbers[x][0] == 0 ? ' ' : '_');
+    }
+    printf("\n");
+    // print second line
+    for (int x = 0; x < MAX_NUMBERS; x++) {
+        printf("%c%c%c ", numbers[x][5] == 0 ? ' ' : '|',
+               numbers[x][6] == 0 ? ' ' : '_', numbers[x][1] == 0 ? ' ' : '|');
+    }
+    printf("\n");
+    // print third line
+    for (int x = 0; x < MAX_NUMBERS; x++) {
+        printf("%c%c%c ", numbers[x][4] == 0 ? ' ' : '|',
+               numbers[x][3] == 0 ? ' ' : '_', numbers[x][2] == 0 ? ' ' : '|');
+    }
+    printf("\n");
 }
